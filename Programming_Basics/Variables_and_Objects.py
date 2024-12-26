826349aa686aca87ecc65fe3afa9b3515338c6dd
premiercourses <premiercoursesllc@gmail.com>
2024-12-26 03:02:27 +0000

    Added the Variables and Objects Lesson

--- Programming_Basics/Variables_and_Objects.py
+++ Programming_Basics/Variables_and_Objects.py
@@ -1,14 +1,19 @@
-# This is a varaiable
-myNumber = 5
+class Variables_and_Objects:
+    def __init__(self):
+      pass
 
-# Here is Another Variable
-myOtherNumber = 10
-
-# this is a variable As well
-myString = "Hello World"
-
-# This too is a variable
-myBoolean = True
-
-# Now lets print them all - You will notice that some of these are print friendly
-print(myNumber, myOtherNumber, myString, myBoolean)
+    def main():
+      # This is a varaiable
+      myNumber = 5
+      
+      # Here is Another Variable
+      myOtherNumber = 10
+      
+      # this is a variable As well
+      myString = "Hello World"
+      
+      # This too is a variable
+      myBoolean = True
+      
+      # Now lets print them all - You will notice that some of these are print friendly
+      print(myNumber, myOtherNumber, myString, myBoolean)
---
+++ main.py
@@ -0,0 +1,6 @@
+from Programming_Basics.Variables_and_Objects import Variables_and_Objects as VO
+
+# Use this file as the entry point to each of the exercises
+# You can comment and uncomment the code in order to reach the correct sections
+
+VO.main()
