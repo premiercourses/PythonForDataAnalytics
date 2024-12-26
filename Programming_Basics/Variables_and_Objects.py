efb63e17b2da7e656334ff36cd407aff1545a7df
premiercourses <premiercoursesllc@gmail.com>
2024-12-26 02:48:15 +0000

    Added the initial file for the Variables and Objects lesson

---
+++ Programming_Basics/Variables_and_Objects.py
@@ -0,0 +1,14 @@
+# This is a varaiable
+myNumber = 5
+
+# Here is Another Variable
+myOtherNumber = 10
+
+# this is a variable As well
+myString = "Hello World"
+
+# This too is a variable
+myBoolean = True
+
+# Now lets print them all - You will notice that some of these are print friendly
+print(myNumber, myOtherNumber, myString, myBoolean)
