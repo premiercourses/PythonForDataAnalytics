a7df96a70f71ebcaa9fae166de062481c5f60ed2
Premier Courses <premiercoursesllc@gmail.com>
2025-01-02 02:27:53 +0000

    Added the arithmetic operators lesson

---
+++ Programming_Basics/Arithmetic_Operators.py
@@ -0,0 +1,36 @@
+class Arithmetic_Operators:
+
+  def __init__(self):
+    pass
+
+  def main():
+    # Addition
+    x = 4
+    y = 6
+
+    sum = x + y
+    print(sum)
+
+    # Subtraction
+    diff = y - x
+    print(diff)
+
+    # Multiplication
+    prod = x * y
+    print(prod)
+
+    # Division
+    quot = y / x
+    print(quot)
+
+    # Modulus
+    mod = y % x
+    print(mod)
+
+    # Exponentiation
+    exp = y**x
+    print(exp)
+
+    # Floor Division
+    floor = y // x
+    print(floor)
